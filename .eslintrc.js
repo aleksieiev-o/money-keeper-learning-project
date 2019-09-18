d503176eb12ecd5846fca95a26e7fef97e09a887
Alexander Alexeev <mandred1991@gmail.com>
2019-09-18 22:55:17 +0300

    edit

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,10 @@ module.exports = {
         '@vue/airbnb',
     ],
     rules: {
-        'indent': ['error', 4, {'SwitchCase': 1}],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'semi': 0,
+        semi: 0,
     },
     parserOptions: {
         parser: 'babel-eslint',
